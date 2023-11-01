55908341c433dbd1eba652850d816707347bce9d
LeKiem <levankiemdev@gmail.com>
2023-11-01 10:03:27 +0700

    #6.2 Đặt Tên Số Ít/Số Nhiều Cho Table Database

--- src/service/userService.js
+++ src/service/userService.js
@@ -26,7 +26,7 @@ const createNewUser = async (email, password, username) => {
   });
   try {
     const [rows, fields] = await connection.execute(
-      `INSERT INTO users (email, password, username) VALUES (?, ?, ?)`,
+      `INSERT INTO user (email, password, username) VALUES (?, ?, ?)`,
       [email, hasPass, username]
     );
   } catch (error) {
@@ -42,7 +42,7 @@ const getUserList = async () => {
     Promise: bluebird
   });
   try {
-    const [rows, fields] = await connection.execute("Select * from users");
+    const [rows, fields] = await connection.execute("Select * from user");
     return rows;
   } catch (error) {}
 };
@@ -56,7 +56,7 @@ const deleteUser = async (id) => {
   });
   try {
     const [rows, fields] = await connection.execute(
-      "Delete  from users WHERE id=?",
+      "Delete  from user WHERE id=?",
       [id]
     );
     return rows;
@@ -73,7 +73,7 @@ const getUserById = async (id) => {
   });
   try {
     const [rows, fields] = await connection.execute(
-      "Select *  from users WHERE id=?",
+      "Select *  from user WHERE id=?",
       [id]
     );
     return rows;
@@ -92,7 +92,7 @@ const updateUserInfo = async (email, username, id) => {
   });
   try {
     const [rows, fields] = await connection.execute(
-      "UPDATE users SET email = ? , username = ? WHERE id =? ",
+      "UPDATE user SET email = ? , username = ? WHERE id =? ",
       [email, username, id]
     );
     return rows;
