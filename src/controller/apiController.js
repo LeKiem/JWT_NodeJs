38c15d101986f309f6654590027d8ef2612f9175
Le Van Kiem <levankiemdev@gmail.com>
2024-01-24 16:02:49 +0700

    #10.2.2 Register Complete

--- src/controller/apiController.js
+++ src/controller/apiController.js
@@ -16,14 +16,14 @@ const handleRegister = async (req, res) => {
     if (!req.body.email || !req.body.phone || !req.body.password) {
       return res.status(200).json({
         EM: "Missing required parameters",
-        EC: "1", //
+        EC: 1, //
         DT: "",
       });
     }
     if (req.body.password && req.body.password.length < 4) {
       return res.status(200).json({
         EM: "Your password must have more than 3 letters",
-        EC: "1", //
+        EC: 1, //
         DT: "",
       });
     }
@@ -38,7 +38,7 @@ const handleRegister = async (req, res) => {
   } catch (e) {
     return res.status(500).json({
       EM: "eror from server",
-      EC: "-1", //
+      EC: -1, //
       DT: "",
     });
   }
