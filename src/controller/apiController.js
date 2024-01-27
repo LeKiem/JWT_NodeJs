c3e8f2f6441b6ec974267678e2f1c206b54e6a0c
Le Van Kiem <levankiemdev@gmail.com>
2024-01-28 01:24:25 +0700

    #11.8 React context Lưu thông tin global

--- src/controller/apiController.js
+++ src/controller/apiController.js
@@ -48,11 +48,14 @@ const handleLogin = async (req, res) => {
   // console.log(req.body);
   try {
     let data = await loginRegisterService.handleUserLogin(req.body);
-    //set cookie
-    res.cookie("jwt", data.DT.access_token, {
-      httpOnly: true,
-      maxAge: 60 * 60 * 1000,
-    });
+    if (data && data.DT && data.DT.access_token) {
+      //set cookie
+      res.cookie("jwt", data.DT.access_token, {
+        httpOnly: true,
+        maxAge: 60 * 60 * 1000,
+      });
+    }
+
     return res.status(200).json({
       EM: data.EM,
       EC: data.EC, //
